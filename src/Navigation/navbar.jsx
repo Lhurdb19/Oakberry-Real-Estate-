dfdfbdb7658e979fc771c852feffda9f921bf0ca
Ejiwumi Basir Abiodun <haryomidey15@gmail.com>
2025-03-25 22:30:41 +0100

    rent updated

--- src/Navigation/navbar.jsx
+++ src/Navigation/navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                 <li className="dropdown" onClick={() => toggleDropdown("home")} >
                     <h4>About ▼</h4>
                     {activeDropdown === "home" && (
-                        <ul className="dropdown-menu">
+                        <ul className="dropdown-menu-box">
                             <Link to="/aboutus" onClick={() => setIsMobile(false)}>About Us</Link>
                             <Link to="/ouragent" onClick={() => setIsMobile(false)}>Our Agents</Link>
                             <Link to="/survey1" onClick={closeDropdown}></Link>
@@ -36,7 +36,7 @@ const Navbar = () => {
                 <li className="dropdown" onClick={() => toggleDropdown("suite")}>
                     <h4>Properties ▼</h4>
                     {activeDropdown === "suite" && (
-                        <ul className="dropdown-menu">
+                        <ul className="dropdown-menu-box">
                             <Link to="/rentproperty" onClick={() => setIsMobile(false)}>Rent Property</Link>
                             <Link to="/saleproperty" onClick={() => setIsMobile(false)}>Sale Property</Link>
                         </ul>
@@ -45,13 +45,13 @@ const Navbar = () => {
                 <li className="dropdown" onClick={() => toggleDropdown("room")}>
                     <h4>Services ▼</h4>
                     {activeDropdown === "room" && (
-                        <ul className="dropdown-menu">
+                        <ul className="dropdown-menu-box">
                             <Link to="/services" onClick={() => setIsMobile(false)}> Our Services</Link>
                             <Link to="/architecturalPlanning" onClick={() => setIsMobile(false)}>Architectural Planning</Link>
                             <Link to="/consultancy" onClick={() => setIsMobile(false)}>Consultancy</Link>
                             <Link to="/interiordesign" onClick={() => setIsMobile(false)}>Interior Design</Link>
                             <Link to="/propertydevelopment" onClick={() => setIsMobile(false)}>Property Development</Link>
-                            <Link to="/generalbuildingsuppliers" onClick={() => setIsMobile(false)}> General Building Suppliers</Link>
+                            <Link to="/generalbuildingsuppliers" onClick={() => setIsMobile(false)}> Building Suppliers</Link>
                         </ul>
                     )}
                 </li>
